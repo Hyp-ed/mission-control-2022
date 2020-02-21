18775a87006ff0fbb58ce6d890345150e92a9bbd
Robertas Norkus <robertas.nork@gmail.com>
2020-02-21 18:57:43 +0000

    Allow connections before opening GUI

--- src/main/java/server/Controller.java
+++ src/main/java/server/Controller.java
@@ -1,96 +1,88 @@
 package server;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import javax.annotation.*;
+import org.json.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
-import org.json.*;
 
-@RestController
+@org.springframework.stereotype.Controller 
 public class Controller {
+  @Autowired
+  private Server server;
 
-    @Autowired
-    private Server server;
+  @Autowired
+  private SimpMessagingTemplate template;
 
-    // starts base station server, and returns if it has connected to pod client or not
-    @RequestMapping(path = "/server", method = RequestMethod.POST)
-    public String postServer() {
-        if (server != null) {
-            Thread serverThread = new Thread(server);
-            serverThread.start();
-            System.out.println("Server started");
-        }
+  @Autowired
+  private TaskScheduler scheduler;
 
-        return String.valueOf(server.isConnected());
+  @PostConstruct
+  public void initialize() {
+    if (server != null) {
+      Thread serverThread = new Thread(server);
+      serverThread.start();
+      System.out.println("Server started");
     }
+  }
 
-    @Autowired
-    private SimpMessagingTemplate template;
+  @MessageMapping("/pullData")
+  @SendTo("/topic/isPodConnected")
+  public void startPingingData() {
+    Thread checkToScheduleThread = new Thread(
+      new Runnable() {
 
-    @Autowired
-    private TaskScheduler scheduler;
-
-    @MessageMapping("/pullData")
-    @SendTo("/topic/isPodConnected")
-    public void startPingingData() {
-        Thread checkToScheduleThread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                while (!server.isConnected()) {
-                    try {
-                        Thread.sleep(200);
-                    }
-                    catch (InterruptedException e) {
-                        System.out.println("Error putting thread to sleep while checking if pod is connected");
-                    }
-                }
-
-                template.convertAndSend("/topic/isPodConnected", "CONNECTED");
-
-                scheduler.scheduleAtFixedRate(() -> pingPodConnectionStatus(), 100);
-                scheduler.scheduleAtFixedRate(() -> pingData(), 100);
-
-                return;  // end thread
+        @Override
+        public void run() {
+          while (!server.isConnected()) {
+            try {
+              Thread.sleep(200);
+            } catch (InterruptedException e) {
+              System.out.println(
+                "Error putting thread to sleep while checking if pod is connected"
+              );
             }
-        });
+          }
 
-        checkToScheduleThread.start();
+          scheduler.scheduleAtFixedRate(() -> pingPodConnectionStatus(), 100);
+          scheduler.scheduleAtFixedRate(() -> pingData(), 100);
+
+          return; // end thread
+        }
+      }
+    );
+
+    checkToScheduleThread.start();
+  }
+
+  @MessageMapping("/sendMessage")
+  @SendTo("/topic/sendMessageStatus")
+  public String sendMessage(String msg) {
+    try {
+      if (server != null && server.isConnected()) {
+        server.sendMessage(msg);
+        return "{\"status\":\"sent msg\", \"message\":" + msg + "}";
+      }
+    } catch (JSONException e) {
+      return "{\"status\":\"error\", \"errorMessage\":\"poorly formed json attempted to be sent to server (probs entered nothing in run_length box)\"}";
     }
 
-    @MessageMapping("/sendMessage")
-    @SendTo("/topic/sendMessageStatus")
-    public String sendMessage(String msg) {
-        try {
-            if (server != null && server.isConnected()) {
-                server.sendMessage(msg);
-                return "{\"status\":\"sent msg\", \"message\":" + msg + "}";
-            }
-        }
-        catch (JSONException e) {
-            return "{\"status\":\"error\", \"errorMessage\":\"poorly formed json attempted to be sent to server (probs entered nothing in run_length box)\"}";
-        }
+    return "{\"status\":\"error\", \"errorMessage\":\"could not send message\"}";
+  }
 
-        return "{\"status\":\"error\", \"errorMessage\":\"could not send message\"}";
+  public void pingPodConnectionStatus() {
+    if (!server.isConnected()) {
+      template.convertAndSend("/topic/isPodConnected", "DISCONNECTED");
+    } else {
+      template.convertAndSend("/topic/isPodConnected", "CONNECTED");
     }
+  }
 
-    public void pingPodConnectionStatus() {
-        if (!server.isConnected()) {
-            template.convertAndSend("/topic/isPodConnected", "DISCONNECTED");
-            System.out.println("DISCONNECTED TO POD");
-        }
-        else {
-            template.convertAndSend("/topic/isPodConnected", "CONNECTED");
-        }
+  public void pingData() {
+    if (server.getMessage() != null) {
+      template.convertAndSend("/topic/podData", server.getMessage().toString());
     }
-
-    public void pingData() {
-        if (server.getMessage() != null) {
-            template.convertAndSend("/topic/podData", server.getMessage().toString());
-        }
-    }
+  }
 }
