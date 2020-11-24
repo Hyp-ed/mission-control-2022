f1e90200b7bb6e4cda3c2097217b32e49545b04f
Robertas <robertas.nork@gmail.com>
2020-11-24 22:22:37 +0200

    Remove commented code

--- frontend/src/components/Gauge/WindowDimensions.js
+++ frontend/src/components/Gauge/WindowDimensions.js
@@ -8,17 +8,17 @@ function getWindowDimensions() {
     };
   }
   
-  export default function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-  
-    useEffect(() => {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-      }
-  
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-  
-    return windowDimensions;
-  }
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
