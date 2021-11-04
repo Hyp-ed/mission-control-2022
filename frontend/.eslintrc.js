5bbbe6152565e77ff03c7f3805bd33e66d20291a
Arjun Naha <arjun.naha@gmail.com>
2021-11-04 15:43:42 +0000

    Add Babel ESLint parser

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: [require.resolve('babel-preset-react-app/prod')],
+    },
+  },
   env: {
-    node: true,
+    browser: true,
     es2021: true,
   },
-  extends: [
-    "airbnb-typescript",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "prettier/react",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint", "simple-import-sort"],
-  ignorePatterns: ["/dist", "/node_modules", " .eslintrc.js", ".huskyrc.js", ".lintstagedrc.js"],
+  extends: ["airbnb", "eslint:recommended", "plugin:prettier/recommended", "prettier/react"],
+  plugins: ["simple-import-sort"],
+  ignorePatterns: ["/dist", "/node_modules", " .eslintrc.js"],
   rules: {
     // Prettier issues are shown as warnings
     "prettier/prettier": "warn",
@@ -48,48 +45,55 @@ module.exports = {
     "no-prototype-builtins": "off",
 
     // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "error",
-
-    // Use function hoisting to improve code readability
-    "no-use-before-define": ["error", { functions: false, classes: true, variables: true }],
-
-    // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    "@typescript-eslint/explicit-function-return-type": [
-      "error",
-      { allowExpressions: true, allowTypedFunctionExpressions: true },
-    ],
-    "@typescript-eslint/no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: true, typedefs: true },
-    ],
-
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    // "import/prefer-default-export": "off",
+    // "import/no-default-export": "error",
 
     "no-underscore-dangle": "off",
-    
+
     // Hoping to use TS
     "react/prop-types": "off",
     //  Define defaults in destructuring
     "react/require-default-props": "off",
+    // We can use normal .js extensions
+    "react/jsx-filename-extension": "off",
+
+    // RULES TO REMOVE AT SOME POINT
+    "no-use-before-define": "off",
+    "no-console": "off",
   },
   overrides: [
     {
-      files: ["**/*.test.[tj]s?(x)"],
-      extends: ["plugin:jest/recommended", "plugin:jest/style"],
-      plugins: ["jest"],
-      rules: {
-        "jest/consistent-test-it": "warn",
-        "jest/lowercase-name": ["warn", { ignore: ["describe", "test"] }],
-        "jest/no-test-return-statement": "error",
-        "jest/prefer-todo": "warn",
-        "jest/require-top-level-describe": "error",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-      },
+      files: ["**/*.ts", "**/*.tsx"],
       env: {
-        "jest/globals": true,
+        browser: true,
+        es2021: true,
+      },
+      extends: [
+        "airbnb-typescript",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+        "prettier/react",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ["@typescript-eslint", "simple-import-sort"],
+      rules: {
+        // Makes no sense to allow type inferrence for expression parameters, but require typing the response
+        "@typescript-eslint/explicit-function-return-type": [
+          "error",
+          { allowExpressions: true, allowTypedFunctionExpressions: true },
+        ],
+        "@typescript-eslint/no-use-before-define": [
+          "error",
+          { functions: false, classes: true, variables: true, typedefs: true },
+        ],
+
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
       },
     },
   ],
